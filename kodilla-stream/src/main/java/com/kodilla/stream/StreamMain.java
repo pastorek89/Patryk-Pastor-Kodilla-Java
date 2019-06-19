6e43f54e6c5f63e78e84718f7286054c3793e124
pastorek89 <pastor.patryk@gmail.com>
2019-06-19 17:18:24 +0200

    BeginningOfStream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +11,7 @@ public class StreamMain {
         poemBeautifier.beautify("Today I'm learning Java", string -> string.toLowerCase());
         poemBeautifier.beautify("Today I'm learning Java", string -> "(^^)" + string + "(^^)");
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
