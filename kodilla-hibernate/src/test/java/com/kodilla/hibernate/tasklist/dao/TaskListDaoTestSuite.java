17ec8882f97bb457a0a09ad5c6b5d74ecbced3a7
pastorek89 <pastor.patryk@gmail.com>
2019-09-19 21:46:01 +0200

    Exercise 17.2 Creating entity - Spring Hibernate

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,38 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("Duties", "Everyday duties");
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTaskLists = taskListDao.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, readTaskLists.size());
+
+        //Clean up
+        int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+
+    }
+}
